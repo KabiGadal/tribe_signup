57592e74dcf5af793c79ceb8b48c284e6052baba
Kabi <gadalkabi@gmail.com>
2022-03-01 08:38:01 +0530

    signup assignment

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import  Login  from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import Home from "./Components/Homepage/Home";
+import {useState} from "react";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  const [user,setLoginUser] = useState({})
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Home />}></Route> */}
+        <Route path="/" element={user && user._id ? <Home /> : <Login setLoginUser={setLoginUser}/> }></Route>
+        <Route path="/login" element={<Login setLoginUser={setLoginUser} />}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+        {/* <Route path="/profile/:username" element={<Profile/>}></Route> */}
+      </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
